9e097ac6fe1b451d38c6608e7dec9ecabb741c13
jacarrichan <772929989@qq.com>
2022-04-03 22:31:49 +0800

    引入zipkin-server

---
+++ cloud-zipkin-server/src/main/java/com/jacarrichan/sck/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,19 @@
+package com.jacarrichan.sck.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @author jacarrichan
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
